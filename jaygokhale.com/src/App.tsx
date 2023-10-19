9ab91608531e1bbfbb1b42c9451dc826dfff818b
Jay Gokhale <gokhale.jay@gmail.com>
2023-10-19 14:50:25 -0700

    Vite setup

---
+++ jaygokhale.com/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Resume from './assets/jgokhale_resume.png'
+
+function App() {
+  return (
+    <object data={Resume} type="image/png" width="100%" height="100%" />
+  )
+}
+
+export default App
